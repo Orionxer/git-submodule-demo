ca76d1a4cd1aab67561ab892bc2f5629a4bf3b3f
beancurd <beancurd-jr@jeritech.com>
2026-08-23 12:04:13 +0800

    ☕️ 修改主仓库

--- example.lua
+++ example.lua
@@ -1,10 +1,10 @@
-package.path = "./third_party/json.lua/?.lua;" .. package.path
+package.path = "./json.lua/?.lua;" .. package.path
 
 local json = require("json")
 
 local value = {
-  project = "git-submodule-demo",
   submodule = "rxi/json.lua",
+  path = "json.lua",
   version = "v0.1.0"
 }
 
